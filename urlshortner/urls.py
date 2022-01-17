e7b32bd01845325cc9109fd16d47984e2ebb417a
Prabhrati Rastogi <84764755+Prabhratirastogi@users.noreply.github.com>
2022-01-17 10:28:53 +0530

    delete change

--- urlshortner/urls.py
+++ urlshortner/urls.py
@@ -16,10 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from django.conf import settings
-from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('shortner.urls')),
-]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+    path('',include('shortner.urls'))
+]
